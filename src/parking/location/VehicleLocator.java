8de72d1797a314f19b15798f5a5fd7e5edd568d0
HarshithaSenadheera <harshithathineth3@gmail.com>
2026-08-28 00:16:10 +0530

    vehicle number and type validation add part

--- src/parking/location/VehicleLocator.java
+++ src/parking/location/VehicleLocator.java
@@ -1,1 +1,34 @@
 package parking.location;
+public class FindMyVehicle
+{
+    //Reference for the main parking lot
+    private ParkingLot parkingLot;
+
+    //Constructor 
+    public FindMyVehicle(ParkingLot parkingLot)
+    {
+        this.parkingLot = parkingLot;
+    }
+    /*Main method to Find my vehicle feature
+    *Inputs : vehicle number
+            : vehicle type
+    *Output : Vehicle location
+            : Error message
+    */
+   public String findVehicle(String vehicleNumber, String vehicleType)
+   {
+    //vehicle number validate
+    if(vehicleNumber == null || vehicleNumber.trim().isEmpty())
+    {
+        return "Invalid information.\n"
+                +"please enter a vehicle number.";
+    }
+    //vehicle type validate
+    if(vehicleType == null || vehicleType.trim().isEmpty())
+    {
+        return "Invalid information.\n"
+                +"please enter a vehicle type.";
+    }
+    vehicleNumber =vehicleNumber.trim();
+    vehicleType = vehicleType.trim();
+   }}
