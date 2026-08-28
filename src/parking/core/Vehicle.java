f36ec00308e5336ba906885bf6015ba59267878b
Arvin Arun Roy <arvinarunroy00@gmail.com>
2026-08-28 19:15:17 +0530

    Add version 1 of Vehicle class

--- src/parking/core/Vehicle.java
+++ src/parking/core/Vehicle.java
@@ -1,1 +1,14 @@
 package parking.core;
+public class Vehicle {
+
+    String vehicleNumber;
+
+    public Vehicle(String vehicleNumber){
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle("ABC-123");
+        System.out.println(v1.vehicleNumber);
+    }
+}
