6b7467979a9d2c00dd95cd1c2f496d91f5e19ede
Arvin Arun Roy <arvinarunroy00@gmail.com>
2026-08-29 16:47:09 +0530

    Debugging

--- src/parking/location/VehicleLocator.java
+++ src/parking/location/VehicleLocator.java
@@ -1,78 +1,70 @@
 package parking.location;
+
 import parking.core.Vehicle;
-import parking.core.ParikngLot;
+import parking.core.ParkingLot;
+import parking.core.ParkingSpace; 
 import parking.core.VehicleClass;
 
-public class VehicleLocator
-{
+public class VehicleLocator {
     private ParkingLot parkingLot;
 
-    //Constructor 
-    public VehicleLocator(ParkingLot parkingLot)
-    {
+    public VehicleLocator(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
     }
-     //Inputs : vehicle number,vehicle type
-     //Output : Vehicle location,Error message
-   public String findVehicle(String vehicleNumber, String vehicleType)
-   {
-    //vehicle number validate
-    if(vehicleNumber == null || vehicleNumber.trim().isEmpty())
-    {
-        return "Invalid information !\nplease enter a vehicle number.";
-    }
-    vehicleNumber = vehicleNumber.trim();
-    //vehicle type validate
-    if(vehicleType == null || vehicleType.trim().isEmpty())
-    {
-        return "Invalid information !\nplease enter a vehicle type.";
-    }
-    vehicleType = vehicleType.trim();
-    //changing the vehicle type to the vehicle class
-    VehicleClass vehicleClass = classifyVehicle(vehicleType);
-    if(vehicleClass == null)
-    {
-        return "Invalid information !\nplease try again.";
-    }
-    //seaching for currently filled parking slots
-    for(ParkingSpace space : parkingLot.getParkingSpaces())
-    {
-        if(space.isOccupied())
-        {
+
+    public String findVehicle(String vehicleNumber, String vehicleType) {
+
+        if (vehicleNumber == null || vehicleNumber.trim().isEmpty()) {
+            return "Invalid information !\nplease enter a vehicle number.";
+        }
+        vehicleNumber = vehicleNumber.trim();
+
+
+        if (vehicleType == null || vehicleType.trim().isEmpty()) {
+            return "Invalid information !\nplease enter a vehicle type.";
+        }
+        vehicleType = vehicleType.trim();
+
+        VehicleClass vehicleClass = classifyVehicle(vehicleType);
+        if (vehicleClass == null) {
+            return "Invalid information !\nplease try again.";
+        }
+
+        for (ParkingSpace space : parkingLot.getParkingSpaces()) {
+            if (space.isOccupied()) {
                 Vehicle vehicle = space.getVehicle();
 
-                if(vehicle.getVehicleNumber().equalsIgnoreCase(vehicleNumber) && vehicle.getVehicleClass() == vehicleClass)
-                {
-                        return "Vehicle Found.\n"
-                        + "Floor :" +space.getFloor()
-                        + "\nParking space :"+space.getSpaceNumber();
+                if (vehicle.getVehicleNumber().equalsIgnoreCase(vehicleNumber) 
+                        && vehicle.getVehicleClass() == vehicleClass) {
+                    return "Vehicle Found.\n"
+                        + "Floor :" + space.getFloor()
+                        + "\nParking space :" + space.getSpaceNumber();
                 }
+            }
         }
+
+        return "Vehicle not found !\n" + "Please check your Vehicle number and the type.";
     }
-    //if there is no matching vehicle is found
-    return "Vehicle not found !\n" + "Please check your Vehicle number and the type.";
-    }
-    private VehicleClass classifyVehicle(String vehicleType)
-    {
-        switch(vehicleType.toLowerCase())
-        {
-                case "suv":
-                case "car":
-                case "jeep":
-                case "pickup truck":
-                case "pickup":
+
+    private VehicleClass classifyVehicle(String vehicleType) {
+        switch (vehicleType.toLowerCase()) {
+            case "suv":
+            case "car":
+            case "jeep":
+            case "pickup truck":
+            case "pickup":
                 return VehicleClass.LIGHT;
 
-                case "motorcycle":
-                case "bike":
-                case "motorbike":
+            case "motorcycle":
+            case "bike":
+            case "motorbike":
                 return VehicleClass.MOTORCYCLE;
 
-                case "truck":
-                case "bus":
+            case "truck":
+            case "bus":
                 return VehicleClass.HEAVY;
 
-                default:
+            default:
                 return null;
         }
     }
